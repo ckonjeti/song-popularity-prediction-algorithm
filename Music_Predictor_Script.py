919638ce51d27769141999c93ab605068b8dde43
ckonjeti1 <ckonjeti1@gmail.com>
2019-03-30 03:09:35 -0400

    update code

--- Music_Predictor_Script.py
+++ Music_Predictor_Script.py
@@ -18,7 +18,7 @@ import csv
 
 genius = lyricsgenius.Genius("Xf0XfBJTZon0Sra2rGV56TAXp6jOUaLJVhmHxqbTW5mp-j6S2NVcmHWSLQ29v0dk")
 
-def songCharacteristics(i, dataWriter, billboardChart):
+def writeSongCharacteristics(i, dataWriter, billboardChart):
     song = billboardChart[i]
     title = billboardChart[i].title
     artist = song.artist
@@ -44,7 +44,7 @@ def getAllSongData(startMonth, startYear, endMonth, endYear, numSongs):
                             billboardChart = billboard.ChartData('hot-100', date = "{}-{:02d}-{}".format(year, month, day))
                             for i in range(0, numSongs):
                                 try:
-                                    songCharacteristics(i, dataWriter, billboardChart)
+                                    writeSongCharacteristics(i, dataWriter, billboardChart)
                                 except:
                                     pass
                         except:
@@ -54,7 +54,7 @@ def getAllSongData(startMonth, startYear, endMonth, endYear, numSongs):
                             billboardChart = billboard.ChartData('hot-100', date = "{}-{}-{}".format(year, month, day))
                             for i in range(0, numSongs):
                                 try:
-                                    songCharacteristics(i, dataWriter, billboardChart)
+                                    writeSongCharacteristics(i, dataWriter, billboardChart)
                                 except:
                                     pass
                         except:
