c0be4131aaa009204634cea2d83fbd8242926a46
ckonjeti1 <ckonjeti1@gmail.com>
2019-03-30 01:56:32 -0400

    iterates through year and month

--- Music_Predictor_Script.py
+++ Music_Predictor_Script.py
@@ -17,41 +17,54 @@ genius = lyricsgenius.Genius("Xf0XfBJTZon0Sra2rGV56TAXp6jOUaLJVhmHxqbTW5mp-j6S2N
 
 
 
-outputFileName = "billboardHot100_Lyrics.csv"
-with open(outputFileName, "a+") as outputFile:
-    dataWriter = csv.writer(outputFile)
-    for year in range(1950, 2020):
-        for month in range (1, 13):
+
+
+for year in range(1959, 2020):
+    for month in range (1, 13):
+        outputFileName = "billboardHot100_Lyrics_{}_{}.csv".format(year, month)
+        with open(outputFileName, "a+") as outputFile:
+            dataWriter = csv.writer(outputFile)
             if month in range(1, 10):
                 try:
-                    billboardChart = billboard.ChartData('hot-100', date = "{}-{:02d}-01").format(year, month)
+                    date = "{}-{:02d}-01".format(year, month) 
+                    billboardChart = billboard.ChartData('hot-100', date = "{}-{:02d}-01".format(year, month))
+                    print("date: " + date)
+                    print(billboardChart.date)
+                    for i in range(0, 0):
+                        try:
+                            song = billboardChart[i]
+                            title = billboardChart[i].title
+                            artist = song.artist
+                            peakpos = song.peakPos
+                            lastpos = song.lastPos
+                            numWeeks = song.weeks
+                            rank = song.rank
+                            isNew = song.isNew
+                            lyrics = genius.search_song(title, artist).lyrics
+                            row = [title, artist, lyrics, peakpos, lastpos, numWeeks, rank, isNew]
+                            dataWriter.writerow(row)
+                        except:
+                            pass
                 except:
                     pass
-            if month in range(10, 13):
+            elif month in range(10, 13):
                 try:
-                    billboardChart = billboard.ChartData('hot-100', date = "{}-{}-01").format(year, month)
+                    billboardChart = billboard.ChartData('hot-100', date = "{}-{}-01".format(year, month))
+                    for i in range(0, 0):
+                        try:
+                            song = billboardChart[i]
+                            title = billboardChart[i].title
+                            artist = song.artist
+                            peakpos = song.peakPos
+                            lastpos = song.lastPos
+                            numWeeks = song.weeks
+                            rank = song.rank
+                            isNew = song.isNew
+                            lyrics = genius.search_song(title, artist).lyrics
+                            row = [title, artist, lyrics, peakpos, lastpos, numWeeks, rank, isNew]
+                            dataWriter.writerow(row)
+                        except:
+                            pass
                 except:
                     pass
-            for i in range(0, 1):
-                try:
-                    song = billboardChart[i]
-                    title = billboardChart[i].title
-                    artist = song.artist
-                    peakpos = song.peakPos
-                    lastpos = song.lastPos
-                    numWeeks = song.weeks
-                    rank = song.rank
-                    isNew = song.isNew
-                    lyrics = genius.search_song(title, artist).lyrics
-                    row = [title, artist, lyrics, peakpos, lastpos, numWeeks, rank, isNew]
-                    dataWriter.writerow(row)
-                except:
-                    pass
-    
-    
-
-        
-    
-
-    
-
+                
