c38792724cc6d4ca7fea34e08b63791432f7cf38
Taylor Smith <taylornsmith1000@gmail.com>
2019-03-30 15:42:24 -0400

    Implemented Dictionary

--- Data_Collection.py
+++ Data_Collection.py
@@ -12,59 +12,80 @@ import lyricsgenius
 #import SpotifyCharts as sCharts
 import csv
 import os
+import textstat
 
 #################################################################
-#Create hashset that updates peakpos, number of weeks in hot-100
+
 #################################################################
-path = "C:/Users/Chaitu Konjeti/SongPopularityPredictionAlgorithm/output"
+path = "C:/Users/Taylor Smith/Desktop/Code/SongPopularityPredictionAlgorithm/output"
 genius = lyricsgenius.Genius("Xf0XfBJTZon0Sra2rGV56TAXp6jOUaLJVhmHxqbTW5mp-j6S2NVcmHWSLQ29v0dk")
 try:  
     os.mkdir(path)
-except :  
+except:  
     pass
 
-def writeSongCharacteristics(i, dataWriter, billboardChart):
-    song = billboardChart[i]
-    title = billboardChart[i].title
-    artist = song.artist
-    peakpos = song.peakPos
-    lastpos = song.lastPos
-    numWeeks = song.weeks
-    rank = song.rank
-    isNew = song.isNew
-    lyrics = genius.search_song(title, artist).lyrics
-    row = [title, artist, lyrics, peakpos, lastpos, numWeeks, rank, isNew]
-    dataWriter.writerow(row)
+def writeSongCharacteristics(i, billboardChart, songs):
+    key = str(billboardChart[i].title + billboardChart[i].artist)
+    
+    if key not in songs:
+        song = billboardChart[i]
+        title = billboardChart[i].title
+        artist = song.artist
+        peakpos = song.peakPos
+        lastpos = song.lastPos
+        numWeeks = song.weeks
+        currentPos = song.rank
+        isNew = song.isNew
+        lyrics = genius.search_song(title, artist).lyrics
+        row = [title, artist, lyrics, peakpos, lastpos, numWeeks, currentPos, isNew]
+        songs[key] = row
+        
+    if key in songs:
+        if billboardChart[i].peakPos != songs.get(key)[3]:
+            newRow = songs.get(key)
+            newRow[3] = billboardChart[i].peakPos
+            songs[key] = newRow
+        if billboardChart[i].weeks != songs.get(key)[5]:
+            newRow = songs.get(key)
+            newRow[5] = billboardChart[i].weeks
+            songs[key] = newRow
+            
 
 def getAllSongData(startMonth, startYear, endMonth, endYear, numSongs):
     for year in range(startYear, endYear + 1):
         for month in range (startMonth, endMonth + 1):
-            outputFileName = "C:/Users/Chaitu Konjeti/SongPopularityPredictionAlgorithm/output/billboardHot100_Lyrics_{}_{}.csv".format(year, month)
-            day = 1
-            while (day <= 31):
-                with open(outputFileName, "a+") as outputFile:
-                    dataWriter = csv.writer(outputFile)
+            outputFileName = "C:/Users/Taylor Smith/Desktop/Code/SongPopularityPredictionAlgorithm/output/billboardHot100_Lyrics_{}_{}.csv".format(year, month)
+            
+            with open(outputFileName, 'a+', newline='') as outputFile:
+                songs = {}
+                day = 1
+                dataWriter = csv.writer(outputFile)
+                
+                while (day <= 31):
                     if month in range(1, 10):
                         try:
                             billboardChart = billboard.ChartData('hot-100', date = "{}-{:02d}-{}".format(year, month, day))
                             for i in range(0, numSongs):
                                 try:
-                                    writeSongCharacteristics(i, dataWriter, billboardChart)
+                                    writeSongCharacteristics(i, billboardChart, songs)
                                 except:
                                     pass
                         except:
                             pass
+                        
                     elif month in range(10, 13):
                         try:
                             billboardChart = billboard.ChartData('hot-100', date = "{}-{}-{}".format(year, month, day))
                             for i in range(0, numSongs):
                                 try:
-                                    writeSongCharacteristics(i, dataWriter, billboardChart)
+                                    writeSongCharacteristics(i, billboardChart, songs)
                                 except:
                                     pass
                         except:
                             pass
-                day += 5
-                
-
-getAllSongData(1, 2009, 2, 2009, 1)
+                    day += 5
+                    
+                for key in songs:
+                    dataWriter.writerow(songs[key])
+           
+getAllSongData(1, 2009, 3, 2009, 1)
